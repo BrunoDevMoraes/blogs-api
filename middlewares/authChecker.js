93355c0b93a6dee7aa666a3d063d338499a92c67
BrunoDevMoraes <brunodevmoraes@gmail.com>
2022-05-09 17:18:31 -0300

    Fix getAllUsers and getUserById

--- middlewares/authChecker.js
+++ middlewares/authChecker.js
@@ -1,7 +1,7 @@
 const { checkToken } = require('../jwt');
 
 const authChecker = (req, res, next) => {
-  const token = req.header.authorization;
+  const token = req.headers.authorization;
   if (!token) return res.status(400).json({ message: 'Token not found' });
   const tokenStatus = checkToken(token);
   if (tokenStatus === false) res.status(400).json({ message: 'Expired or invalid token' });
