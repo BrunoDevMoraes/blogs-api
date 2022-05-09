029099ab90ae6c6ef521d116649e0e379ae64135
BrunoDevMoraes <brunodevmoraes@gmail.com>
2022-05-09 19:59:47 -0300

    Fix 02

--- middlewares/loginChecker.js
+++ middlewares/loginChecker.js
@@ -1,10 +1,13 @@
 const loginChecker = (req, res, next) => {
   const object = req.body;
-  if (!object.email) return res.status(400).json({ message: '"email is required' });
+  console.log(object.email);
+  if (object.email === undefined) return res.status(400).json({ message: '"email" is required' });
   if (object.email.length === 0) {
     return res.status(400).json({ message: '"email" is not allowed to be empty' });
   }
-  if (!object.password) return res.status(400).json({ message: '"password" is required' });
+  if (object.password === undefined) {
+    return res.status(400).json({ message: '"password" is required' });
+  }
   if (object.password.length === 0) {
     return res.status(400).json({ message: '"password" is not allowed to be empty' });
   }
